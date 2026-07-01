8a70986846a44b9f923aefa9df9c3cafd6188708
pruthvigaddameedi <31732505+pruthvigaddameedi@users.noreply.github.com>
2026-07-01 14:45:00 +0530

    Update GetUsersHandler.cs

--- UserDirectory.Application/UseCases/GetUsers/GetUsersHandler.cs
+++ UserDirectory.Application/UseCases/GetUsers/GetUsersHandler.cs
@@ -1,1 +1,12 @@
+using UserDirectory.Application.Interfaces;
+
+namespace UserDirectory.Application.UseCases.GetUsers;
+
+public class GetUsersHandler
+{
+    private readonly IUserRepository _repo;
+    public GetUsersHandler(IUserRepository repo) => _repo = repo;
+    public Task<List<UserDirectory.Domain.Entities.User>> Handle(CancellationToken ct = default) =>
+        _repo.GetAllAsync(ct);
+}
 
